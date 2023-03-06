ff69683ce5487abbd7edc3b499d1db275e6eaea9
devkjoon <devkjoon@gmail.com>
2023-03-06 12:29:20 -0500

    clientside src js completed

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,9 +28,9 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new StaleWhileRevalidate({
-    cacheName: "asset-cache",
+  ({ request }) => request.destination === "image",
+  new CacheFirst({
+    cacheName: "assets",
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
