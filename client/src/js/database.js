eee156bfae960ed5252452fb747937012771ec80
devkjoon <devkjoon@gmail.com>
2023-03-08 11:34:57 -0500

    completed appliation

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,23 +7,20 @@ const initdb = async () =>
         console.log("jate database already exists");
         return;
       }
-      db.createObjectStore("jate", {
-        keyPath: "id", 
-        autoIncrement: true
-      });
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
       console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDB = await openDB('jate', 1);
-  const tx = jateDB.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log(result);
-}
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async (e) => {
